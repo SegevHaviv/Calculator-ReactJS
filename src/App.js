07ebf4f2cbe5e866021251fb5119a878de6ce03d
segevhaviv <segevhaviv@gmail.com>
2019-02-18 22:34:15 +0200

    Added auto-shrinking display box.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,66 @@
 import React, { useState } from "react";
 import "./App.css";
 
+class AutoScalingText extends React.Component {
+  state = {
+    scale: 1
+  };
+
+  componentDidUpdate() {
+    const { scale } = this.state
+
+    const node = this.node
+    const parentNode = node.parentNode
+
+    const availableWidth = parentNode.offsetWidth
+    const actualWidth = node.offsetWidth
+    const actualScale = availableWidth / actualWidth
+
+    if (scale === actualScale)
+      return
+
+    if (actualScale < 1) {
+      this.setState({ scale: actualScale })
+    } else if (scale < 1) {
+      this.setState({ scale: 1 })
+    }
+  }
+
+  render() {
+    const { scale } = this.state
+
+    return (
+      <div
+        className="auto-scaling-text"
+        style={{ transform: `scale(${scale},${scale})` }}
+        ref={node => this.node = node}
+      >{this.props.children}</div>
+    )
+  }
+}
+
+function CalculatorDisplay(props) {
+
+  const language = navigator.language || 'en-US'
+  let formattedValue = parseFloat(props.value).toLocaleString(language, {
+    useGrouping: true,
+    maximumFractionDigits: 6
+  })
+
+  // Add back missing .0 in e.g. 12.0
+  const match = props.value.match(/\.\d*?(0*)$/)
+
+  if (match)
+    formattedValue += (/[1-9]/).test(match[0]) ? match[1] : match[0]
+
+  return (
+    <div {...props} className="calculator-display">
+      <AutoScalingText>{formattedValue}</AutoScalingText>
+    </div>
+  )
+}
+
+
 function App(props) {
   const [displayValue, setDisplayValue] = useState("0");
   const [prevValue, setPrevValue] = useState(null);
@@ -17,8 +77,9 @@ function App(props) {
 
   return (
     <div className="root">
+
       <div id="wrapper">
-        <div className="result-display">{displayValue}</div>
+        <CalculatorDisplay value={displayValue} />
 
         {/*first row */}
         <div className="row">
@@ -111,9 +172,6 @@ function App(props) {
 
     setWaitingForOperator(true);
     setOperator(operatorInserted);
-
-
-
   }
 }
 
