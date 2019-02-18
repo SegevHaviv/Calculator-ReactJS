102b6de87a169875caa5bda82fa4523013e599b8
segevhaviv <segevhaviv@gmail.com>
2019-02-18 22:07:13 +0200

    Added operators functionality.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import "./App.css";
 
 function App(props) {
   const [displayValue, setDisplayValue] = useState("0");
-  // const [operandInserted, setOperandInserted] = useState(false);
-  // const [prevValue, setPrevValue] = useState(0);
+  const [prevValue, setPrevValue] = useState(null);
+  const [operator, setOperator] = useState(null);
+  const [waitingForOperator, setWaitingForOperator] = useState(false);
+
+  const CalculatorOperations = {
+    "/": (prevValue, nextValue) => prevValue / nextValue,
+    "X": (prevValue, nextValue) => prevValue * nextValue,
+    "+": (prevValue, nextValue) => prevValue + nextValue,
+    "-": (prevValue, nextValue) => prevValue - nextValue,
+    "=": (prevValue, nextValue) => nextValue
+  };
+
   return (
     <div className="root">
       <div id="wrapper">
@@ -12,110 +22,98 @@ function App(props) {
 
         {/*first row */}
         <div className="row">
-          <button
-            className="button first-row-button"
-            onClick={() => onClearInput()}
-          >
-            AC
-          </button>
-          <button
-            className="button first-row-button"
-            onClick={() => onToggleSign()}
-          >
-            ±
-          </button>
-          <button className="button first-row-button">%</button>
-          <button
-            className="button operator-button"
-            onClick={() => onOperandInput("/")}
-          >
-            ÷
-          </button>
+          <button className="button first-row-button" onClick={() => onClearInput()}>AC</button>
+          <button className="button first-row-button" onClick={() => onToggleSign()}>±</button>
+          <button className="button first-row-button" onClick={() => clearLastDigit()}><i className="fas fa-backspace"></i></button>
+          <button className="button operator-button" onClick={() => onOperatorInput("/")}>÷</button>
         </div>
 
         {/*second row */}
         <div className="row">
           {createButtons([7, 8, 9])}
-          <button
-            className="button operator-button"
-            onClick={() => onOperandInput("x")}
-          >
-            x
-          </button>
+          <button className="button operator-button" onClick={() => onOperatorInput("X")}>x</button>
         </div>
 
         {/*third row */}
         <div className="row">
           {createButtons([4, 5, 6])}
-          <button
-            className="button operator-button"
-            onClick={() => onOperandInput("-")}
-          >
-            -
-          </button>
+          <button className="button operator-button" onClick={() => onOperatorInput("-")}>-</button>
         </div>
 
         {/*fourth row */}
         <div className="row">
           {createButtons([1, 2, 3])}
-          <button
-            className="button operator-button"
-            onClick={() => onOperandInput("+")}
-          >
-            +
-          </button>
+          <button className="button operator-button" onClick={() => onOperatorInput("+")}>+</button>
         </div>
 
         {/*fifth row */}
         <div className="row">
-          <button
-            className="button"
-            style={{ flex: "2.3" }}
-            onClick={() => onDigitInput(0)}
-          >
-            0
-          </button>
-          <button className="button" onClick={() => onDotInput()}>
-            .
-          </button>
-          <button className="button operator-button">=</button>
+          <button className="button" style={{ flex: "2.3" }} onClick={() => onDigitInput(0)}> 0 </button>
+          <button className="button" onClick={() => onDotInput()}>.</button>
+          <button className="button operator-button" onClick={() => onOperatorInput("=")}>=</button>
         </div>
+
       </div>
     </div>
   );
 
   function createButtons(buttonsData) {
     return buttonsData.map(x => (
-      <button className="button" key={x} onClick={() => onDigitInput(x)}>
-        {x}
-      </button>
+      <button className="button" key={x} onClick={() => onDigitInput(String(x))}>{x}</button>
     ));
   }
 
+  function clearLastDigit() {
+    setDisplayValue(displayValue.substring(0, displayValue.length - 1) || '0');
+  }
+
   function onDigitInput(digit) {
-    setDisplayValue(
-      displayValue === "0" ? digit : displayValue + String(digit)
-    );
+    let digitInsertedValue;
+    if (waitingForOperator) {
+      setDisplayValue(digit);
+      setWaitingForOperator(false);
+    } else {
+      digitInsertedValue = displayValue === "0" ? digit : displayValue + String(digit);
+      setDisplayValue(digitInsertedValue);
+    }
   }
 
   function onClearInput() {
     setDisplayValue("0");
+    setPrevValue(null);
+    setOperator(null);
+    setWaitingForOperator(false);
   }
 
   function onToggleSign() {
-    let numberValue = Number.parseFloat(displayValue);
-    numberValue = -numberValue;
-    setDisplayValue(String(numberValue));
+    const signToggledValue = displayValue.includes("-") ? displayValue.substring(1) : "-" + displayValue;
+    setDisplayValue(signToggledValue);
   }
 
   function onDotInput() {
-    setDisplayValue(
-      displayValue.includes(".") ? displayValue : displayValue + "."
-    );
+    const dotInsertedValue = displayValue.includes(".") ? displayValue : displayValue + ".";
+    setDisplayValue(dotInsertedValue);
   }
 
-  function onOperandInput(operand) {
-    console.log(operand);
+  function onOperatorInput(operatorInserted) {
+    console.log(operatorInserted);
+    const inputValue = parseFloat(displayValue);
+
+    if (prevValue == null) {
+      setPrevValue(inputValue);
+    } else if (operator) {
+      const currentValue = prevValue;
+      const newValue = CalculatorOperations[operator](currentValue, inputValue);
+
+      setPrevValue(newValue);
+      setDisplayValue(String(newValue));
+    }
+
+    setWaitingForOperator(true);
+    setOperator(operatorInserted);
+
+
+
   }
 }
 
