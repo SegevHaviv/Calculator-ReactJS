861ccb980d647340d0fdb5b7841e5bb643537c8f
segevhaviv <segevhaviv@gmail.com>
2019-02-20 21:08:05 +0200

    Updates

---
+++ precache-manifest.f2dec3fa3d10a69d22cefcd454b6c86f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16766c5738868822571e",
+    "url": "/Calculator-ReactJS/static/css/main.6b7b8772.chunk.css"
+  },
+  {
+    "revision": "16766c5738868822571e",
+    "url": "/Calculator-ReactJS/static/js/main.16766c57.chunk.js"
+  },
+  {
+    "revision": "3e308d659ce06c17ea22",
+    "url": "/Calculator-ReactJS/static/js/runtime~main.3e308d65.js"
+  },
+  {
+    "revision": "3f967fe085aeb6371aba",
+    "url": "/Calculator-ReactJS/static/js/2.3f967fe0.chunk.js"
+  },
+  {
+    "revision": "744d3449630867de063de35c5fc83909",
+    "url": "/Calculator-ReactJS/index.html"
+  }
+];
